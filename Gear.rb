5d141b974a8bf50c2c5256edab1ad9b93610a0dd
Yuliya Nedyalkova <yuliya.nedyalkova777@gmail.com>
2017-09-07 14:34:54 +0100

    refactoring: embedded Wheel in Gear by using Struct class

--- Gear.rb
+++ Gear.rb
@@ -1,13 +1,24 @@
 class Gear
-  attr_reader :chaining, :cog
-  def initialize(chaining, cog)
+  attr_reader :chaining, :cog, :wheel
+  def initialize(chaining, cog, rim, tire)
     @chaining = chaining
     @cog = cog
+    @wheel = Wheel.new(rim, tire)
   end
 
   def ratio
     chaining/cog.to_f
   end
+
+  def gear_inches
+    ratio * wheel.diameter
+  end
+
+  Wheel = Struct.new(:rim, :tire) do
+    def diameter
+      rim + (tire * 2)
+    end
+  end
 end
 
-puts Gear.new(52, 12).ratio
+puts Gear.new(52, 11, 26, 1.5).gear_inches
