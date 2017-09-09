95932ade3bc0cb2ec4cded88317b0a2640200074
Yuliya Nedyalkova <yuliya.nedyalkova777@gmail.com>
2017-09-09 12:36:34 +0100

    Further refactoring: Removed argument dependencies in Gear

--- Gear.rb
+++ Gear.rb
@@ -1,13 +1,15 @@
+require_relative 'wheel'
+
 class Gear
-  attr_reader :chaining, :cog, :wheel
-  def initialize(chaining, cog, wheel=nil)
-    @chaining = chaining
-    @cog = cog
-    @wheel = wheel
+  attr_reader :chainring, :cog, :wheel
+  def initialize(args)
+    @chainring = args[:chainring]
+    @cog = args [:cog]
+    @wheel = args [:wheel]
   end
 
   def ratio
-    chaining/cog.to_f
+    chainring/cog.to_f
   end
 
   def gear_inches
@@ -15,24 +17,7 @@ class Gear
   end
 end
 
-class Wheel
-  attr_reader :rim, :tire
-
-  def initialize(rim, tire)
-    @rim = rim
-    @tire = tire
-  end
-
-  def diameter
-      rim + (tire * 2)
-  end
-
-  def circumference
-    diameter * Math::PI
-  end
-end
-
-@wheel = Wheel.new(26, 1.5)
-puts @wheel.circumference
-
-puts Gear.new(52, 11, @wheel).gear_inches
+puts Gear.new(
+  :chainring  => 52,
+  :cog        => 11,
+  :wheel      => Wheel.new(26, 1.5)).gear_inches
